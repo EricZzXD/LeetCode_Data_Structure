bd0b7fb294693ad33134960682737b9841b19560
EricZ <yuhuazhao52@gmail.com>
2020-10-06 00:40:41 +1100

    'LeetCode_StringAndArray_To_be_Continues'

--- src/com/company/LeetCodeStringAndArray/LeetCodeStringAndArray_2DArray.java
+++ src/com/company/LeetCodeStringAndArray/LeetCodeStringAndArray_2DArray.java
@@ -1,13 +1,28 @@
 package com.company.LeetCodeStringAndArray;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class LeetCodeStringAndArray_2DArray {
     public static int main(){
         System.out.println("\nLeetCode_StringAndArray_2DArray: ");
 
+        // LeetCode Link: https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1167/
+        int [][] findDiagonalOrder_2DArray = {{1,2,3},{4,5,6},{7,8,9}};
+        findDiagonalOrder(findDiagonalOrder_2DArray);
+
+        // LeetCode Link: https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1168/
+        int [][] spiralOrder_Matrix = {{1,2,3},{4,5,6},{7,8,9}};
+//        int [][] spiralOrder_Matrix = {{3},{2}};
+        spiralOrder(spiralOrder_Matrix);
+
         return 0;
     }
 
     /*
+        LeetCode Link: https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1167/
+
         Given a matrix of M x N elements (M rows, N columns), return all elements of the matrix in diagonal order as shown in the below image.
 
         Example:
@@ -24,10 +39,71 @@ public class LeetCodeStringAndArray_2DArray {
             Note:
                 The total number of elements of the given matrix will not exceed 10,000.
      */
-
     public static int[] findDiagonalOrder(int[][] matrix) {
-        int [] arr = new int[3];
+        int arrCounter = 0;
+        int [] arr = new int[matrix.length*matrix[0].length];
+        // To be continues ------------------------------------------------
 
         return arr;
+    }
+
+    /*
+        LeetCode Link: https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1168/
+
+        Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+        Example 1:
+            Input:
+            [
+             [ 1, 2, 3 ],
+             [ 4, 5, 6 ],
+             [ 7, 8, 9 ]
+            ]
+            Output: [1,2,3,6,9,8,7,4,5]
+
+        Example 2:
+            Input:
+            [
+              [1, 2, 3, 4],
+              [5, 6, 7, 8],
+              [9,10,11,12]
+            ]
+            Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+     */
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List <Integer> list = new LinkedList<Integer>();
+
+        // Check if the matrix is empty
+        if(matrix.length==0 || matrix[0].length==0){
+            return list;
+        }
+
+        int Column1 = 0, Column2=matrix.length-1;
+        int Row1 = 0, Row2=matrix[0].length-1;
+
+        while(Column1 <= Column2 && Row1<=Row2){
+            // Append the row into the list
+            for(int r=Row1;r<=Row2;r++){
+                list.add(matrix[Column1][r]);
+            }
+            for(int c=Column1+1;c<=Column2;c++){
+                list.add(matrix[c][Row2]);
+            }
+            if(Row1<Row2 && Column1<Column2) {
+                for (int r = Row2-1; r > Row1; r--) {
+                    list.add(matrix[Column2][r]);
+                }
+                for (int c = Column2; c > Column1; c--) {
+                    list.add(matrix[c][Row1]);
+                }
+            }
+            Column1++;
+            Column2--;
+            Row1++;
+            Row2--;
+        }
+        System.out.println("spiralOrder: " + list);
+        return list;
     }
 }
--- src/com/company/LeetCodeStringAndArray/LeetCodeStringAndArray_Introduction.java
+++ src/com/company/LeetCodeStringAndArray/LeetCodeStringAndArray_Introduction.java
@@ -18,6 +18,18 @@ public class LeetCodeStringAndArray_Introduction {
         int [] plusOne_array = {9};
         plusOne(plusOne_array);
 
+        // LeetCode -cn -----------------------------
+
+        // LeetCode Link: https://leetcode-cn.com/leetbook/read/array-and-string/cxqdh/
+        int [] searchInsert_array ={1,3,5,6};
+        int searchInsert_target=5;
+        searchInsert(searchInsert_array, searchInsert_target);
+
+        // LeetCode Link: https://leetcode-cn.com/leetbook/read/array-and-string/c5tv3/
+        int [][] merge_matrix = {{1,3}, {2,6},{8,10},{15,18}};
+        merge(merge_matrix);
+
+
         return 0;
     }
 
@@ -175,5 +187,67 @@ public class LeetCodeStringAndArray_Introduction {
         }
         System.out.println("plusOne: "+Arrays.toString(digits));
         return digits;
+    }
+
+    /*
+        LeetCode Link: https://leetcode-cn.com/leetbook/read/array-and-string/cxqdh/
+
+        给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+        你可以假设数组中无重复元素。
+
+        示例 1:
+            输入: [1,3,5,6], 5
+            输出: 2
+
+        示例 2:
+            输入: [1,3,5,6], 2
+            输出: 1
+
+        示例 3:
+            输入: [1,3,5,6], 7
+            输出: 4
+
+        示例 4:
+            输入: [1,3,5,6], 0
+            输出: 0
+     */
+    public static int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(target<=nums[i]){
+                System.out.println("searchInsert: " + i);
+                return i;
+            }
+        }
+        System.out.println("searchInsert: " + nums.length);
+        return nums.length;
+    }
+
+    /*
+        LeetCode Link: https://leetcode-cn.com/leetbook/read/array-and-string/c5tv3/
+        给出一个区间的集合，请合并所有重叠的区间。
+
+        示例 1:
+            输入: intervals = [[1,3],[2,6],[8,10],[15,18]]
+            输出: [[1,6],[8,10],[15,18]]
+            解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+
+        示例 2:
+            输入: intervals = [[1,4],[4,5]]
+            输出: [[1,5]]
+            解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+
+        注意：输入类型已于2019年4月15日更改。 请重置默认代码定义以获取新方法签名。   
+
+        提示：
+            intervals[i][0] <= intervals[i][1]
+
+     */
+    public static int[][] merge(int[][] intervals) {
+        for(int i=0;i<intervals.length;i++){
+            for(int j=i+1;j<intervals[i].length;j++){
+                // To be continuous
+            }
+        }
+        return intervals;
     }
 }
